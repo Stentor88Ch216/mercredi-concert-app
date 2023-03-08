f0e1ca848712ec99b31627a4e15bfd51ba1d5589
Thomas DOUCHE <Thomas.douche@aol.com>
2023-03-08 16:44:54 +0100

    correction boutton ajouter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Typography from "@mui/joy/Typography";
 import Stack from "@mui/joy/Stack";
-import IconButton from "@mui/joy/IconButton";
-//import Add from '@mui/icons-material/Add';
+//import IconButton from "@mui/joy/IconButton";
+import { Button } from "@mui/joy";
 
 let ggdocLink = "https://docs.google.com/spreadsheets/d/1vDRUs4xFZJfYTO6hJqE01zg45uxBouDJV13BeTg4RaE/edit#gid=0";
 
@@ -9,10 +9,13 @@ let ggdocLink = "https://docs.google.com/spreadsheets/d/1vDRUs4xFZJfYTO6hJqE01zg
 export default function Header() {
     return (
         <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ width: 1 }}>
+            
             <Typography level="h5" fontWeight="bold">MERCREDI CONCERT</Typography>
-            <IconButton component="a" href={ggdocLink} target="_blank" color="info">
-                {/**<Add />**/}
-            </IconButton>
+            
+            <Button href={ggdocLink} target="_blank" color="info" size="sm">
+                Ajouter
+            </Button>
+
         </Stack>
         
     );
